605e147abe99e3f4c7eebcd83848125693d2acb9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-15 18:20:07 -0500

    switching to separate data store module

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var initCrashReporter = require('crash-reporter-middleware');
 var la = require('lazy-ass');
 var check = require('check-more-types');
 
-var AppKey = require('./src/models/app-keys');
+var dataStoreInit = require('crash-store-db');
+la(check.fn(dataStoreInit), 'expected crash store fn', dataStoreInit);
 
 initCrashReporter(getConfigured, app)
   .then(function (crashMiddleware) {
@@ -17,18 +18,12 @@ initCrashReporter(getConfigured, app)
       app.use(crashMiddleware);
     }
 
-    var dbInit = require('./src/db');
-    dbInit.then(function (db) {
+    dataStoreInit().then(function (store) {
+      console.log('got data store with methods', Object.keys(store));
+
       var port = getConfigured('PORT');
       http.createServer(app).listen(port);
       console.log('listening at port %d', port);
-
-      AppKey.saveDummy().then(function (key) {
-        console.log('created dummy application key');
-        console.log(key);
-      }, function (err) {
-        console.error(err);
-      });
 
     }, function (err) {
       console.error(err);
