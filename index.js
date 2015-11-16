ae02975f5c214b751584076519089370a61bf7d7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-15 19:20:35 -0500

    feat(api): Receivig POST requests via HTTP
    closes #2

--- index.js
+++ index.js
@@ -23,8 +23,26 @@ function checkCrashEmitter(emitter) {
   crashEmitter = emitter;
 }
 
+function connectEmitterToStore() {
+  la(check.object(crashEmitter), 'missing crash event emitter', crashEmitter);
+  la(check.object(dataStore), 'missing data store', dataStore);
+  la(check.fn(dataStore.storeException), 'missing data store save method', dataStore);
+
+  crashEmitter.on('crash', function (crashInfo) {
+    console.log('crash emitter on crash handler');
+    la(check.object(crashInfo), 'invalid crashInfo', crashInfo);
+    dataStore.storeException(crashInfo)
+      .catch(function (err) {
+        console.error('could not store crash info');
+        console.error(err);
+      });
+  });
+}
+
 function finalHandler(err) {
+  console.log('error');
   console.error(err);
+  console.error(err.stack);
   process.exit(-1);
 }
 
@@ -32,4 +50,5 @@ dataStoreInit()
   .then(checkDataStore)
   .then(listenerInit)
   .then(checkCrashEmitter)
+  .then(connectEmitterToStore)
   .catch(finalHandler);
--- src/listener.js
+++ src/listener.js
@@ -4,10 +4,97 @@ var getConfigured = config.get.bind(config);
 var la = require('lazy-ass');
 var check = require('check-more-types');
 
+var url = require('url');
+// handle data encoded in json or text body
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var textParser = bodyParser.text();
+
+function respondToInvalid(res) {
+  res.writeHead(400, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Invalid request\n');
+}
+
+function respondToInvalidCrash(res) {
+  res.writeHead(400, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Invalid crash information\n');
+}
+
+function isValid(req, parsed) {
+  return req.method === 'POST' &&
+    parsed &&
+    parsed.query &&
+    check.has(parsed.query, 'apiKey');
+}
+
+function isValidCrash(crashInfo) {
+  return check.object(crashInfo) &&
+    check.has(crashInfo, 'Details');
+}
+
+function writeResponse(res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  });
+  res.end('Got crash info\n');
+}
+
+function saveCrashReport(crashEmitter, req) {
+  var crashInformation = req.body;
+  if (check.unemptyString(crashInformation.Details)) {
+    console.log('parsing', crashInformation.Details);
+    crashInformation.Details = JSON.parse(crashInformation.Details);
+  }
+  var title = crashInformation.Details &&
+    crashInformation.Details.Error &&
+    crashInformation.Details.Error.Message;
+  if (!title &&
+    check.unemptyString(crashInformation.Details)) {
+    title = crashInformation.Details;
+  }
+  console.log('saving crash report with title "%s"', title);
+  crashEmitter.emit('crash', crashInformation);
+}
+
 function initListener() {
   var http = require('http');
   var initCrashReporter = require('crash-reporter-middleware');
   var app = require('express')();
+
+  var events = require('events');
+  var crashEmitter = new events.EventEmitter();
+
+  app.use(function filterInvalid(req, res, next) {
+    var parsed = url.parse(req.url, true);
+    console.log('%s - %s query', req.method, parsed.href, parsed.query);
+    if (!isValid(req, parsed)) {
+      return respondToInvalid(res);
+    }
+    next();
+  });
+
+  app.use(jsonParser);
+  app.use(textParser);
+
+  app.use(function filterInvalidCrashes(req, res, next) {
+    if (!isValidCrash(req.body)) {
+      return respondToInvalidCrash(res);
+    }
+    next();
+  });
+
+  app.use(function (req, res, next) {
+    saveCrashReport(crashEmitter, req);
+    writeResponse(res);
+    next();
+  });
 
   function useCrash(crashMiddleware) {
     if (crashMiddleware) {
@@ -20,9 +107,8 @@ function initListener() {
     var port = getConfigured('PORT');
     http.createServer(app).listen(port);
     console.log('listening at port %d', port);
-
-    var events = require('events');
-    var crashEmitter = new events.EventEmitter();
+    console.log('to test use httpie https://github.com/jkbrzt/httpie');
+    console.log('http POST <host>:<port>/?apiKey=demo foo=bar Details=\'"nice"\'');
     return crashEmitter;
   }
 
